2e7701ef5008496e1d13f85bdbaa4671f5ba649f
quinaeng <quinaeng@gmail.com>
2022-03-28 19:22:02 +0900

    update

--- function/menu.py
+++ function/menu.py
@@ -2,3 +2,5 @@ def menu(wine,entree,dessert):
   return {"wine":wine,"entree":entree,"dessert":dessert}
 
 print(menu("chardonnay","chiken","cake"))
+# {'wine': 'chardonnay', 'entree': 'chiken', 'dessert': 'cake'}
+
